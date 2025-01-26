8567340117b039c6417ebeec54b6f7b0edbe6ba2
Chris Johnson <johnsoctbus@gmail.com>
2025-01-26 14:19:56 -0500

    forward working as aspected! WIP onbackward

--- src/utils/virtualization/virtualizationer.ts
+++ src/utils/virtualization/virtualizationer.ts
@@ -5,7 +5,7 @@ import {
 } from "../../types/virtualization"
 
 // TODO: this.data isn't actually being used to recycling information
-// TODO: new items aren't starting at the scroll position (bottom); they're replacing what's at the bottom
+// BUG: new items aren't starting at the scroll position (bottom); they're replacing what's at the bottom
 export default class Virtualizationer {
     data: Map<number, Message>;
     elementPool: HTMLElement[];
@@ -44,7 +44,7 @@ export default class Virtualizationer {
 
     private backwardCallback() {
         this._page--
-        this.getData(this.requestLimit, this._page)
+        this.getData(this.requestLimit, this._page, "up")
     }
 
     private createNode(message: Message): HTMLElement {
@@ -54,7 +54,7 @@ export default class Virtualizationer {
 
     private forwardCallback() {
         this._page++
-        this.getData(this.requestLimit, this._page)
+        this.getData(this.requestLimit, this._page, "down")
     }
 
     private getBottomObserver(): HTMLElement {
@@ -71,7 +71,7 @@ export default class Virtualizationer {
         return document.getElementById("VirtualList")
     }
 
-    private async getData(limit = this.requestLimit, page: number): Promise<void> {
+    private async getData(limit = this.requestLimit, page: number, direction: string): Promise<void> {
         return this.props.getData(limit, page).then((messages: Message[]) => {
             const listContainer = this.getVirtualListContainer()
 
@@ -94,7 +94,7 @@ export default class Virtualizationer {
                 console.log(`"this.root" was null; could not append fragment`)
             }
 
-            this.updateElementsPosition()
+            this.updateElementsPosition(direction)
             this.updateTopObserverPosition()
             this.updateBottomObserverPosition()
         })
@@ -174,17 +174,40 @@ export default class Virtualizationer {
         }
     }
 
-    private updateElementsPosition(): void {
-        //  TODO: when recycling, only update the position of the recycled half
+    private calculateElementPositionDownwards(el: HTMLElement, prevEl: HTMLElement) {
+        const prevHeight = prevEl?.clientHeight || 0
+        const prevY = Number(prevEl?.getAttribute("data-offset")) || 0
+        const yPos = (prevHeight + prevY).toString()
+
+        el.setAttribute("data-offset", yPos)
+        el.style.transform = `translateY(${yPos}px)`
+    }
+
+    private calculateElementPositionUpwards(el: HTMLElement, prevEl: HTMLElement) {
+        const prevY = Number(prevEl?.getAttribute("data-offset")) || 0
+        const newY = (prevY - el.clientHeight).toString()
+
+        el.setAttribute("data-offset", newY)
+        el.style.transform = `translateY(${newY}px)`
+    }
+
+    // BUG: doesn't take direction into consideration (currently only goes forward)
+    private updateElementsPosition(direction: string): void {
+        // NOTE: only update the most recently updated items
+        // WARN: this function can't be called outside of getData, becauase it assumes it
+        // is immediately following items being added to `this.elementsPool`
         this.elementPool.forEach((el, index) => {
-            const prevHeight = this.elementPool[index - 1]?.clientHeight || 0
-            const prevY = Number(this.elementPool[index - 1]?.getAttribute("data-offset")) || 0
-            const yPos = (prevHeight + prevY).toString()
-
-            //console.log(prevHeight, prevY, yPos, index, this.elementPool[index - 1])
-
-            el.setAttribute("data-offset", yPos)
-            el.style.transform = `translateY(${yPos}px)`
+            if (direction === "down") {
+                if (index >= (this.elementPool.length - 1 - this.requestLimit)) {
+                    this.calculateElementPositionDownwards(el, this.elementPool[index - 1])
+                }
+            }
+            else {
+                // Need to address elementsPool in reverse... 
+                if (index < this.requestLimit) {
+                    this.calculateElementPositionUpwards(el, this.elementPool[index + 1])
+                }
+            }
         })
     }
 
